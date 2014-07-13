07e94e6391b45083a14594a7f116b27d139d4279
Geoff Adams <geoff@dianode.net>
2014-07-13 22:12:37 +0100

    Enable grunt completions

--- home/.zsh/exports.zsh
+++ home/.zsh/exports.zsh
@@ -5,3 +5,6 @@ export JAVA_HOME=$(/usr/libexec/java_home)
 export SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M -Duser.timezone=GMT"
 
 export PATH=$PATH:/Applications/adt-bundle-mac-x86_64-20140321/sdk/platform-tools
+
+if which grunt > /dev/null; then eval "$(grunt --completion=zsh -)"; fi
+
