c52fbf418e27d792eeb9a6fa78e5e23df89a2964
Geoff Adams <geoff@dianode.net>
2014-07-14 21:45:32 +0100

    Restore exports for environment detection variables

--- home/.zsh/environment.zsh
+++ home/.zsh/environment.zsh
@@ -1,26 +1,26 @@
 os_name=$(uname)
 
-IS_LINUX=0
+export IS_LINUX=0
 if [[ "$os_name" == 'Linux' ]]; then
-    IS_LINUX=1
+    export IS_LINUX=1
 fi
 
-IS_MAC=0
+export IS_MAC=0
 if [[ "$os_name" == 'Darwin' ]]; then
-    IS_MAC=1
+    export IS_MAC=1
 fi
 
-HAS_BREW=0
+export HAS_BREW=0
 if [[ -x `which brew` ]]; then
-    HAS_BREW=1
+    export HAS_BREW=1
 fi
 
-HAS_APT=0
+export HAS_APT=0
 if [[ -x `which apt-get` ]]; then
-    HAS_APT=1
+    export HAS_APT=1
 fi
 
-HAS_YUM=0
+export HAS_YUM=0
 if [[ -x `which yum` ]]; then
-    HAS_YUM=1
+    export HAS_YUM=1
 fi
--- home/.zsh/machines.zsh
+++ home/.zsh/machines.zsh
@@ -1,11 +1,11 @@
 hostname=$(hostname -s)
 
-IS_HOME_MACHINE=0
+export IS_HOME_MACHINE=0
 if [[ "$hostname" == "macwarrior" ]]; then
-  IS_HOME_MACHINE=1
+  export IS_HOME_MACHINE=1
 fi
 
-IS_WORK_MACHINE=0
+export IS_WORK_MACHINE=0
 if [[ "$hostname" == "mc-s083179" ]]; then
-  IS_WORK_MACHINE=1
+  export IS_WORK_MACHINE=1
 fi
