c7fd4dc1df9b818d40f58ae1eab726b34bd4c1b9
Geoff Adams <geoff@dianode.net>
2014-12-15 13:13:02 +0000

    Update Java, Maven and sbt options to working defaults

--- home/.zsh/work/maven.zsh
+++ home/.zsh/work/maven.zsh
@@ -1,15 +1,22 @@
-export MAVEN_OPTS="-Xms256m -Xmx512m
-  -Djavax.net.ssl.keyStore=$HOME/Projects/certs/dev.bbc.co.uk.p12 \
-    -Djavax.net.ssl.keyStorePassword=spanglyhorsecoconut \
-      -Djavax.net.ssl.keyStoreType=PKCS12 \
-        -Djavax.net.ssl.trustStore=$HOME/.subversion/jssecacerts \
-        "
+export JAVA_OPTS="
+  -Xms256m -Xmx512m
+  -Djavax.net.ssl.keyStore=$HOME/Documents/certs/dev.bbc.co.uk.p12 \
+  -Djavax.net.ssl.keyStorePassword=spanglyhorsecoconut \
+  -Djavax.net.ssl.keyStoreType=PKCS12 \
+  -Djavax.net.ssl.trustStore=$HOME/Documents/certs/jssecacerts \
+  "
 
 # Comment out the following if you aren't on Reith
-#export MAVEN_OPTS="$MAVEN_OPTS \
-#  -Dhttp.proxyHost=www-cache.reith.bbc.co.uk -Dhttp.proxyPort=80 \
-#  -Dhttps.proxyHost=www-cache.reith.bbc.co.uk -Dhttps.proxyPort=80 \
-#  -Dhttp.nonProxyHosts=localhost|national.core.bbc.co.uk|*.sandbox.dev.bbc.co.uk \
-#  -Dhttps.nonProxyHosts=localhost|national.core.bbc.co.uk|*.sandbox.dev.bbc.co.uk \
-#"
+export JAVA_OPTS="
+  $MAVEN_OPTS \
+  -Dhttp.proxyHost=www-cache.reith.bbc.co.uk
+  -Dhttp.proxyPort=80 \
+  -Dhttps.proxyHost=www-cache.reith.bbc.co.uk
+  -Dhttps.proxyPort=80 \
+  -Dhttp.nonProxyHosts=localhost|national.core.bbc.co.uk|*.sandbox.dev.bbc.co.uk|127.0.0.1 \
+  -Dhttps.nonProxyHosts=localhost|national.core.bbc.co.uk|*.sandbox.dev.bbc.co.uk|127.0.0.1 \
+  "
 
+export MAVEN_OPTS="$JAVA_OPTS"
+export SBT_OPTIONS="$MAVEN_OPTS -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m"
+export SERVER_ENV="sandbox"
--- home/.zshenv
+++ home/.zshenv
@@ -3,6 +3,7 @@ PERSONAL_ZSH=$HOME/.zsh
 source $PERSONAL_ZSH/environment.zsh
 source $PERSONAL_ZSH/machines.zsh
 source $PERSONAL_ZSH/location.zsh
+source $PERSONAL_ZSH/work/maven.zsh
 
 HISTSIZE=10000
 SAVEHIST=9000
@@ -12,6 +13,6 @@ EDITOR=vim
 HELPDIR=/usr/local/share/zsh/helpfiles
 
 JAVA_HOME=$(/usr/libexec/java_home)
-SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M -Duser.timezone=GMT"
+#SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M -Duser.timezone=GMT"
 
 PATH=$HOME/.rbenv/bin:$HOME/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Applications/adt-bundle-mac-x86_64-20140321/sdk/platform-tools:$PATH
