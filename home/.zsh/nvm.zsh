f91e9094700f8e8d63c57bf1a24591a47ad64882
Geoff Adams <geoff@dianode.net>
2018-07-01 10:03:52 +0100

    Tolerate non-Mac systems when setting up nvm

--- home/.zsh/nvm.zsh
+++ home/.zsh/nvm.zsh
@@ -1,5 +1,9 @@
 function nvm() {
   export NVM_DIR="$HOME/.nvm"
-  . "$(brew --prefix nvm)/nvm.sh"
+  if [ hash brew 2>/dev/null ]; then
+    . "$(brew --prefix nvm)/nvm.sh"
+  else
+    [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+  fi
   nvm "$@"
 }
