00e8be55208f69d30aa016cb5509f3c6971b5a26
Geoff Adams <geoff.adams@evergreensmartpower.co.uk>
2018-09-04 15:05:49 +0100

    Fix Homebrew detection step in nvm.zsh

--- home/.zsh/rc/nvm.zsh
+++ home/.zsh/rc/nvm.zsh
@@ -1,6 +1,6 @@
 function nvm() {
   export NVM_DIR="$HOME/.nvm"
-  if [ hash brew 2>/dev/null ]; then
+  if hash brew 2>/dev/null; then
     . "$(brew --prefix nvm)/nvm.sh"
   else
     [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
