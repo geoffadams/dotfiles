61e36aa9d8ed98240954ce2ab6dfd6bb306475d5
Geoff Adams <geoff@dianode.net>
2019-04-03 19:31:45 +0100

    Install VSCode extensions

---
+++ mac/vscode-extensions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+code --install-extension chenxsan.vscode-standardjs
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eg2.tslint
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension rebornix.ruby
+code --install-extension vscodevim.vim
+code --install-extension eriklynd.json-tools
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension oliversturm.fix-json
+code --install-extension dbankier.vscode-quick-select
