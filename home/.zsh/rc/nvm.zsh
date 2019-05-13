90faf47183950225ba7dc72f772e32a2413063a2
Geoff Adams <geoff.adams@evergreensmartpower.co.uk>
2019-05-13 14:42:34 +0100

    Ensure nvm runs before any conventional node invocation, see https://www.growingwiththeweb.com/2018/01/slow-nvm-init.html

--- home/.zsh/rc/nvm.zsh
+++ home/.zsh/rc/nvm.zsh
@@ -1,9 +1,18 @@
-function nvm() {
+export NVM_SCRIPT_DIR="$HOME/.nvm"
+if hash brew 2>/dev/null; then
+  # export NVM_DIR=$(brew --prefix nvm)
+  # Hard-code this instead for terminal load speed.
+  export NVM_SCRIPT_DIR="/usr/local/opt/nvm"
+fi
+
+if [ -s "$NVM_SCRIPT_DIR/nvm.sh" ] && [ ! "$(whence -w __init_nvm)" = function ]; then
   export NVM_DIR="$HOME/.nvm"
-  if hash brew 2>/dev/null; then
-    . "$(brew --prefix nvm)/nvm.sh"
-  else
-    [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-  fi
-  nvm "$@"
-}
+  declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack')
+  function __init_nvm() {
+    for i in "${__node_commands[@]}"; do unalias $i; done
+    source "$NVM_SCRIPT_DIR"/nvm.sh
+    unset __node_commands
+    unset -f __init_nvm
+  }
+  for i in "${__node_commands[@]}"; do alias $i='__init_nvm && '$i; done
+fi
