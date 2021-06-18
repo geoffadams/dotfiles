e6f3c0159ca438d2f88359ede50be2b081b369f6
Geoff Adams <geoff@dianode.net>
2021-06-18 07:57:00 +0100

    Drop Flux install and settings

--- mac/flux.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-defaults write ~/Library/Preferences/org.herf.Flux.plist wakeTime -int 420
-defaults write ~/Library/Preferences/org.herf.Flux.plist transitionSpeed -int 1
-defaults write ~/Library/Preferences/org.herf.Flux.plist wakeNotifyDisable -int 1
-defaults write ~/Library/Preferences/org.herf.Flux.plist disableFullscreen -int 1
-defaults write ~/Library/Preferences/org.herf.Flux.plist lateColorTemp -float 2700
-defaults write ~/Library/Preferences/org.herf.Flux.plist customNightColorTemp -float 3500
