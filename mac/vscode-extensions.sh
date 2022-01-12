5f7bc738cde2cb66c8d31ad8a2ebd04dfc5c2857
Geoff Adams <geoff.adams@acast.com>
2022-01-12 16:40:23 +0000

    Update VSCode extension list to include Go + ergonomic extensions, drop TSLint

--- mac/vscode-extensions.sh
+++ mac/vscode-extensions.sh
@@ -2,13 +2,18 @@
 
 code --install-extension chenxsan.vscode-standardjs
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension eg2.tslint
 code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
 code --install-extension ms-vscode.cpptools
-code --install-extension PeterJausovec.vscode-docker
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension rebornix.ruby
 code --install-extension vscodevim.vim
 code --install-extension eriklynd.json-tools
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension oliversturm.fix-json
 code --install-extension dbankier.vscode-quick-select
+code --install-extension eamodio.gitlens
+code --install-extension golang.go
+code --install-extension vsciot-vscode.vscode-arduino
+code --install-extension albert.tabout
+code --install-extension dotjoshjohnson.xml
