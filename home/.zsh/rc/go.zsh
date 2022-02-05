dd2c0977dbb1c59dafc66338ec479af780e8fae0
Geoff Adams <geoff@dianode.net>
2022-02-05 20:39:30 +0000

    Remove Golang environment variables

--- home/.zsh/rc/go.zsh
+++
@@ -1,5 +0,0 @@
-export GOBIN=/usr/local/Cellar/go/1.2.1/libexec/bin
-export GOROOT=/usr/local/Cellar/go/1.2.1/libexec
-export GOPATH=/Users/geoff/Projects/go
-
-export PATH=$PATH:/usr/local/opt/go/libexec/bin
