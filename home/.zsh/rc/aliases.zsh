c9d094de6672790725e0e4941cca27f44bcd0137
Geoff Adams <geoff.adams@acast.com>
2022-06-27 16:22:58 +0100

    Add docker-shell-into-image to spin up a container with shell entrypoint

--- home/.zsh/rc/aliases.zsh
+++ home/.zsh/rc/aliases.zsh
@@ -70,6 +70,7 @@ fi
 alias docker-image-prune='docker rmi $(docker images -f "dangling=true" -q)'
 alias docker-clean='docker rm $(docker ps -a -q) && docker volume rm $(docker volume ls -q)'
 alias docker-pull-all="docker images | grep -v REPOSITORY | awk '{print \$1\":\"\$2}' | xargs -L1 docker pull"
+alias docker-shell-into-image="docker run --rm -it --entrypoint sh $1"
 
 # Python
 alias py="python"
