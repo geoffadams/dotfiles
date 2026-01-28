ec83ddf76a1317ee6159bf92007b99a07270ea76
Geoff Adams <geoff@dianode.net>
2026-01-28 18:02:14 +0000

    nvim: fzf-lua keymap tweaks

--- home/.config/nvim/lua/workspace.lua
+++ home/.config/nvim/lua/workspace.lua
@@ -1,12 +1,12 @@
 require('mini.bufremove').setup()
 
 require("fzf-lua").setup()
-vim.api.nvim_set_keymap("n", "<C-\\>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-k>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-o>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-?>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-l>", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]], {})
+vim.api.nvim_set_keymap("n", "<C-f>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
 vim.api.nvim_set_keymap("n", "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
 
 require("fzf-lua").utils.info(
-  "|<C-\\> buffers|<C-p> files|<C-g> grep|<C-l> live grep|<C-k> builtin|<F1> help|")
+  "|<C-o> buffers|<C-p> files|<C-f> grep|<C-g> live grep|<C-?> builtin|<F1> help|")
