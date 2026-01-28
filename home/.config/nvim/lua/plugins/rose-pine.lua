1a6d90760b8746a933ed0478a2f2ec9034569cd4
Geoff Adams <geoff@dianode.net>
2026-01-28 17:30:30 +0000

    nvim: don't lazy-load theme

--- home/.config/nvim/lua/plugins/rose-pine.lua
+++ home/.config/nvim/lua/plugins/rose-pine.lua
@@ -1,8 +1,8 @@
--- lua/plugins/rose-pine.lua
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+  "rose-pine/neovim",
+  name = "rose-pine",
+  lazy = false,
+  config = function()
+    vim.cmd("colorscheme rose-pine")
+  end
 }
