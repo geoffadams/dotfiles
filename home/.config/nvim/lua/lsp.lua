82f13da42f9fd3a231584e04624896dd39d77ffb
Geoff Adams <geoff@dianode.net>
2026-01-30 23:14:33 +0000

    nvim: add conform to run formatters

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -2,9 +2,10 @@ require("mason").setup()
 vim.lsp.enable("basedpyright")
 vim.lsp.enable("emmylua_ls")
 vim.lsp.config("bashls", {
-  filetypes = {"bash", "sh", "zsh", ".zshrc", ".zshenv"}
+    filetypes = { "bash", "sh", "zsh" },
 })
 vim.lsp.enable("bashls")
+
 require("mason-tool-installer").setup({
     ensure_installed = {
         "emmylua_ls",
@@ -15,5 +16,19 @@ require("mason-tool-installer").setup({
         "isort",
         "bash-language-server",
         "beautysh",
+    },
+})
+
+require("conform").setup({
+    formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "ruff", "isort" },
+        bash = { "beautysh" },
+        sh = { "beautysh" },
+        zsh = { "beautysh" },
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
     },
 })
