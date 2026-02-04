930aa209bd18661f9c545b548d8ee4e9651f7808
Geoff Adams <geoff.adams@acast.com>
2026-02-04 12:55:51 +0000

    nvim: lazy-load fzf-lua with key mapping trigger

--- home/.config/nvim/lua/plugins/fzf-lua.lua
+++ home/.config/nvim/lua/plugins/fzf-lua.lua
@@ -10,4 +10,12 @@ return {
     ---@diagnostic disable: missing-fields
     opts = {},
     ---@diagnostic enable: missing-fields
+    keys = {
+        { "<C-o>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]] },
+        { "<C-?>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]] },
+        { "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]] },
+        { "<C-g>", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]] },
+        { "<C-f>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]] },
+        { "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]] },
+    },
 }
--- home/.config/nvim/lua/workspace.lua
+++ home/.config/nvim/lua/workspace.lua
@@ -1,15 +1,5 @@
 require("mini.bufremove").setup()
 
-require("fzf-lua").setup()
-vim.api.nvim_set_keymap("n", "<C-o>", [[<Cmd>lua require"fzf-lua".buffers()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-?>", [[<Cmd>lua require"fzf-lua".builtin()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-p>", [[<Cmd>lua require"fzf-lua".files()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-g>", [[<Cmd>lua require"fzf-lua".live_grep()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<C-f>", [[<Cmd>lua require"fzf-lua".grep_project()<CR>]], {})
-vim.api.nvim_set_keymap("n", "<F1>", [[<Cmd>lua require"fzf-lua".help_tags()<CR>]], {})
-
-require("fzf-lua").utils.info("|<C-o> buffers|<C-p> files|<C-f> grep|<C-g> live grep|<C-?> builtin|<F1> help|")
-
 require("gitsigns").setup({
     on_attach = function(bufnr)
         local gitsigns = require("gitsigns")
