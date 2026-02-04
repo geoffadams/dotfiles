6d3604703208f9889c6698683a1a0d72df24c384
Geoff Adams <geoff.adams@acast.com>
2026-02-04 09:56:08 +0000

    nvim: filter diagnostics to everything for active buffer, and project-wide errors only

--- home/.config/nvim/lua/plugins/trouble.lua
+++ home/.config/nvim/lua/plugins/trouble.lua
@@ -1,16 +1,36 @@
 return {
     "folke/trouble.nvim",
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {
+        warn_no_results = false,
+        open_no_results = true,
+        modes = {
+            major_diagnostics = {
+                mode = "diagnostics", -- inherit from diagnostics mode
+                filter = {
+                    any = {
+                        buf = 0, -- current buffer
+                        {
+                            severity = vim.diagnostic.severity.ERROR, -- errors only
+                            -- limit to files in the current project
+                            function(item)
+                                return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+                            end,
+                        },
+                    },
+                },
+            },
+        },
+    },
     cmd = "Trouble",
     keys = {
         {
             "<leader>xx",
-            "<cmd>Trouble diagnostics toggle<cr>",
+            "<cmd>Trouble major_diagnostics toggle<cr>",
             desc = "Diagnostics (Trouble)",
         },
         {
             "<leader>xX",
-            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            "<cmd>Trouble major_diagnostics toggle filter.buf=0<cr>",
             desc = "Buffer Diagnostics (Trouble)",
         },
         {
