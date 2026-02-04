ac85756cae1081a136070550fb2f8c04d2996a9c
Geoff Adams <geoff.adams@acast.com>
2026-02-04 09:54:18 +0000

    nvim: switch ts_ls out for vtsls

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -5,13 +5,54 @@ vim.lsp.config("bashls", {
     filetypes = { "bash", "sh", "zsh" },
 })
 vim.lsp.enable("bashls")
-vim.lsp.enable("ts_ls")
+vim.lsp.config("vtsls", {
+    settings = {
+        typescript = {
+            inlayHints = {
+                parameterNames = { enabled = "all" },
+                parameterTypes = { enabled = true },
+                variableNames = { enabled = true },
+                variableTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                enumMemberValues = { enabled = true },
+            },
+            suggest = {
+                completeFunctionCalls = true,
+            },
+            tsserver = {
+                log = "/Users/geoff.adams/.local/state/nvim/lsp/vtsls.log",
+                experimental = {
+                    enableProjectDiagnostics = true,
+                },
+            },
+            updateImportsOnFileMove = { enabled = "always" },
+        },
+        vtsls = {
+            autoUseWorkspaceTsdk = true,
+        },
+    },
+})
+vim.lsp.enable("vtsls")
 vim.lsp.config("eslint", {
     settings = {
         workingDirectory = { mode = "location" },
     },
 })
 vim.lsp.enable("eslint")
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Enable inlay hints",
+    callback = function(event)
+        local id = vim.tbl_get(event, "data", "client_id")
+        local client = id and vim.lsp.get_client_by_id(id)
+        if client == nil or not client.supports_method("textDocument/inlayHint") then
+            return
+        end
+
+        vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+    end,
+})
 
 require("mason-tool-installer").setup({
     ensure_installed = {
@@ -23,7 +64,7 @@ require("mason-tool-installer").setup({
         "isort",
         "bash-language-server",
         "beautysh",
-        "typescript-language-server",
+        "vtsls",
         "eslint-lsp",
     },
 })
@@ -46,7 +87,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
             bufnr = args.buf,
             lsp_format = "fallback",
             filter = function(client)
-                return client.name ~= "ts_ls"
+                return client.name ~= "vtsls"
             end,
         })
     end,
