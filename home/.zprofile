1bba5a2918809e4f6b08560efd939df399b4347a
Geoff Adams <geoff.adams@acast.com>
2026-02-17 11:39:35 +0000

    zsh: load nvm for all login shells, only lazy-load in interactive shells

---
+++ home/.zprofile
@@ -0,0 +1,4 @@
+# load additional config
+for f in $PERSONAL_ZSH/profile.d/*.zsh(N); do
+    . $f
+done
---
+++ home/.zsh/profile.d/nvm.zsh
@@ -0,0 +1,20 @@
+export NVM_SCRIPT_DIR="$HOME/.nvm"
+if hash brew 2>/dev/null; then
+    export NVM_SCRIPT_DIR=$(brew --prefix nvm)
+fi
+
+if [[ -o interactive ]]; then
+    if [ -s "$NVM_SCRIPT_DIR/nvm.sh" ] && [ ! "$(whence -w __init_nvm)" = function ]; then
+        export NVM_DIR="$HOME/.nvm"
+        declare -a __node_commands=('nvm' 'node' 'npm' 'yarn' 'gulp' 'grunt' 'webpack')
+        function __init_nvm() {
+            for i in "${__node_commands[@]}"; do unalias $i; done
+            source "$NVM_SCRIPT_DIR"/nvm.sh
+            unset __node_commands
+            unset -f __init_nvm
+        }
+        for i in "${__node_commands[@]}"; do alias $i='__init_nvm && '$i; done
+    fi
+else
+    source "$NVM_SCRIPT_DIR"/nvm.sh
+fi
