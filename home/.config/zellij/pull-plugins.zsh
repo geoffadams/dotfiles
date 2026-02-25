10874f00ab6c85f12459e4fcc612d0e5900c0ca8
Geoff Adams <geoff.adams@acast.com>
2026-02-25 18:14:09 +0000

    zellij: add zsm for session management

--- home/.config/zellij/pull-plugins.zsh
+++ home/.config/zellij/pull-plugins.zsh
@@ -2,3 +2,4 @@ cd plugins
 wget -N "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm"
 wget -N "https://github.com/dj95/zjstatus/releases/latest/download/zjframes.wasm"
 wget -N "https://github.com/karimould/zellij-forgot/releases/latest/download/zellij_forgot.wasm"
+wget -N "https://github.com/liam-mackie/zsm/releases/latest/download/zsm.wasm"
