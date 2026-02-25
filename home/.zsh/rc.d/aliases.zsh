d4f0f0b6b0d153e1fa059782179a27ecca22c777
Geoff Adams <geoff.adams@acast.com>
2026-02-25 18:22:34 +0000

    zsh: overdue alias clear-out

--- home/.zsh/rc.d/aliases.zsh
+++ home/.zsh/rc.d/aliases.zsh
@@ -1,76 +1,38 @@
-# Navigation and files
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias bk='cd $OLDPWD'
-
-if [[ $IS_MAC -eq 1 ]]; then
-  alias ls="gls -Fh --color -N --group-directories-first"
-else
-  alias ls="ls -Fh --color -N --group-directories-first"
-fi
+# basics
+alias ls="ls -Fh --color -N --group-directories-first"
 alias ll="ls -Al --time-style=long-iso"
-
 alias mkdir='mkdir -pv'
 
-if [[ $IS_MAC -eq 1 ]]; then
-  alias t="trash"
-fi
-
+# shell history
 alias sync-history="fc -AI && fc -R"
 alias reload-history="fc -R"
 
-# --preserve-root
-# alias rm='rm -I --preserve-root'
-# alias chown='chown --preserve-root'
-# alias chmod='chmod --preserve-root'
-# alias chgrp='chgrp --preserve-root'
-
-# Common settings for tools
+# colorise the world
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
-## Utilities
+# utilities
 alias now='date +"%T"'
 alias nowtime=now
 alias nowdate='date +"%d-%m-%Y"'
-
 alias sha1='openssl sha1'
-alias path='echo -e ${PATH//:/\\n}'
+alias diru='du -h -d 1 | sort -h'
 
 whatsonport() {
-  lsof -i tcp:$*
+    lsof -i tcp:$*
 }
 
-if [[ $IS_MAC -eq 1 ]]; then
-  pbfile() {
-    cat $1 | pbcopy
-  }
-fi
+# mac utilities
+if [ is_mac ]; then
+    alias t="trash"
+    pbfile() {
+        cat $1 | pbcopy
+    }
 
-alias directory-size='du -h -d 1 | sort -h'
-
-# Mac specific utilities
-if [[ $IS_MAC -eq 1 ]]; then
-  # OS X Quick Look
-  alias ql='qlmanage -p 2>/dev/null'
-
-  # open current directory in OS X Finder
-  alias oo='open .'
-
-  # display SMART status of hard drive
-  alias smart='diskutil info disk0 | grep SMART'
-
-  # refresh brew by upgrading all outdated casks
-  alias refreshbrew='brew outdated | while read cask; do brew upgrade $cask; done'
-
-  # rebuild Launch Services to remove duplicate entries on Open With menu
-  alias rebuildopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.fram ework/Support/lsregister -kill -r -domain local -domain system -domain user'
-
-  # use GNU sed instead of BSD sed
-  alias sed="gsed"
-
+    # GNU versions, not BSD versions
+    alias sed="gsed"
+    unalias ls && alias ls="gls -Fh --color -N --group-directories-first"
 fi
 
 # use neovim instead of built-in vim
@@ -81,8 +43,3 @@ alias docker-image-prune='docker rmi $(docker images -f "dangling=true" -q)'
 alias docker-clean='docker rm $(docker ps -a -q) && docker volume rm $(docker volume ls -q)'
 alias docker-pull-all="docker images | grep -v REPOSITORY | awk '{print \$1\":\"\$2}' | xargs -L1 docker pull"
 alias docker-shell-into-image="docker run --rm -it --entrypoint sh $1"
-
-# Python
-alias py="python"
-alias py2="python"
-alias py3="python3"
