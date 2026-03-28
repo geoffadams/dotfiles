6df6770b12b6557ba779280b6bddb14763b552b1
Geoff Adams <geoff@dianode.net>
2026-03-28 11:27:46 +0000

    nvim: QoL tweaks for .urls and help buffers

--- home/.config/nvim/lua/filetypes.lua
+++ home/.config/nvim/lua/filetypes.lua
@@ -1,11 +1,26 @@
 vim.filetype.add({
+    extension = {
+        urls = function()
+            return "urls", function()
+                vim.opt_local.commentstring = "# %s"
+            end
+        end,
+    },
     filename = {
         [".homesickrc"] = "ruby",
     },
 })
+
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "kdl" },
     callback = function()
         vim.treesitter.start()
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "help" },
+    callback = function(opts)
+        vim.keymap.set("n", "gd", "<C-]>", { silent = true, buffer = opts.buf })
     end,
 })
