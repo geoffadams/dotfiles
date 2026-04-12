ee983ea1e2439f406e5e5ebb80bb58ceee87d022
Geoff Adams <geoff@dianode.net>
2026-04-12 15:36:55 +0100

    nvim: enable undotree and difftool

--- home/.config/nvim/lua/interface.lua
+++ home/.config/nvim/lua/interface.lua
@@ -144,3 +144,14 @@ require("fzf-lua").register_ui_select(function(ui_opts, items)
         },
     }
 end)
+
+-- undotree
+vim.cmd("packadd nvim.undotree")
+vim.keymap.set("n", "<leader>tu", function()
+    require("undotree").open({
+        command = math.floor(vim.api.nvim_win_get_width(0) / 3) .. "vnew",
+    })
+end, { desc = "Undotree toggle" })
+
+-- difftool
+vim.cmd("packadd nvim.difftool")
