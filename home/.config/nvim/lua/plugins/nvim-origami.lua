7b94e5d56676093987f0f3f75011c7dc554670e6
Geoff Adams <geoff@dianode.net>
2026-04-12 14:44:59 +0100

    nvim: tweaked autofold settings

--- home/.config/nvim/lua/plugins/nvim-origami.lua
+++ home/.config/nvim/lua/plugins/nvim-origami.lua
@@ -1,9 +1,13 @@
 return {
     "chrisgrieser/nvim-origami",
     event = "VeryLazy",
-    opts = {}, -- required even when using default config
-
-    -- recommended: disable vim's auto-folding
+    opts = {
+        autoFold = {
+            kinds = { "imports" },
+        },
+    },
     init = function()
+        vim.opt.foldlevel = 99
+        vim.opt.foldlevelstart = 99
     end,
 }
