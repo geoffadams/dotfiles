5c0ac1da85a26f1fc3402fd4fa384e7762b00fb5
Geoff Adams <geoff@dianode.net>
2026-04-18 18:53:33 +0100

    zsh: fzf+zoxide together, forever

--- home/.zsh/rc.d/fzf.zsh
+++ home/.zsh/rc.d/fzf.zsh
@@ -1,3 +1,17 @@
 if [[ $(command -v fzf) ]]; then
     source <(fzf --zsh)
+
+    export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+
+    if [[ $(command -v fd) ]]; then
+        # fd for path completion
+        _fzf_compgen_path() {
+            fd --hidden --follow --exclude ".git" . "$1"
+        }
+
+        # fd for file completion
+        _fzf_compgen_dir() {
+            fd --type d --hidden --follow --exclude ".git" . "$1"
+        }
+    fi
 fi
--- home/.zsh/rc.d/zoxide.zsh
+++ home/.zsh/rc.d/zoxide.zsh
@@ -1,3 +1,22 @@
 if [[ $(command -v zoxide) ]]; then
     eval "$(zoxide init zsh)"
+
+    if [[ $(command -v fzf) ]]; then
+        function zoxide_fzf() {
+            local orig_buffer=$LBUFFER
+            local selection
+            selection=$(zoxide query --list | fzf --height 40% --reverse) || {
+                LBUFFER=$orig_buffer
+                zle redisplay
+                return 0
+            }
+
+            if [[ -n "$selection" ]]; then
+                LBUFFER="cd $selection"
+                zle redisplay
+            fi
+        }
+        zle -N zoxide_fzf
+        bindkey '^O' zoxide_fzf
+    fi
 fi
