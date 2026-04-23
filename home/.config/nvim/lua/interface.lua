1dbff9df033e43ec28a1bd1b42ad42052e4ab18b
Geoff Adams <geoff@dianode.net>
2026-04-23 18:47:25 +0100

    nvim: fix copy/paste over SSH

--- home/.config/nvim/lua/interface.lua
+++ home/.config/nvim/lua/interface.lua
@@ -48,6 +48,41 @@ vim.o.showcmd = true -- display partial commands
 vim.o.showmode = false -- don't display current mode
 
 -- clipboard
+if vim.env.SSH_TTY ~= nil then
+    vim.notify("osc52 enabled")
+    local osc52 = require("vim.ui.clipboard.osc52")
+
+    local function copy_reg(reg)
+        local orig = osc52.copy(reg)
+        return function(lines, regtype)
+            -- Write to Vim's internal register
+            vim.fn.setreg(reg, table.concat(lines, "\n"), regtype)
+
+            -- Send OSC52 to local clipboard
+            orig(lines, regtype)
+            orig(lines, regtype)
+        end
+    end
+
+    vim.g.clipboard = {
+        name = "OSC 52 with register sync",
+        copy = {
+            ["+"] = copy_reg("+"),
+            ["*"] = copy_reg("*"),
+        },
+        -- Do NOT use OSC52 paste, just use internal registers
+        paste = {
+            ["+"] = function()
+                return vim.fn.getreg("+"), "v"
+            end,
+            ["*"] = function()
+                return vim.fn.getreg("*"), "v"
+            end,
+        },
+    }
+else
+    vim.notify("osc52 not enabled")
+end
 vim.opt.clipboard:append("unnamedplus")
 
 -- pretty paths in titles
