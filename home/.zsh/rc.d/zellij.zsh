9f95db18f17bcb66ab692762fd3da724ab778a9b
Geoff Adams <geoff@dianode.net>
2026-04-23 23:30:26 +0100

    zsh: zellij session completions

--- home/.zsh/rc.d/zellij.zsh
+++ home/.zsh/rc.d/zellij.zsh
@@ -7,24 +7,26 @@ fi
 
 if [[ $(command -v zellij) ]]; then
     alias zz="zellij"
-    alias zzl="zellij ls"
-    alias zzd="zellij d"
+    alias zzl="zellij ls -r"
 
     if [[ $(command -v fzf) ]]; then
         zza() {
+            local session
             if [[ -z "$1" ]]; then
                 session=$(zellij ls -s | fzf --height=~40% --layout=reverse --info=inline --border --margin=1)
             fi
             zellij a -c "$session"
         }
         zzs() {
+            local host
+            local session
             host=$1
             if [[ -z "$host" ]]; then
                 host=$(grep -E '^Host [^\*]' ~/.ssh/config | awk '{print $2}' | fzf --height=~40% --layout=reverse --info=inline --border --margin=1)
             fi
             session=$2
             if [[ -z "$session" ]]; then
-                session=$(ssh $host -t "zellij ls -s" 2>/dev/null | fzf --height=~40% --layout=reverse --info=inline --border --margin=1)
+                session=$(ssh $host -t "zellij ls -rs" 2>/dev/null | fzf --height=~40% --layout=reverse --info=inline --border --margin=1)
             fi
             ssh $host -t "zellij a -c $session"
         }
@@ -35,13 +37,49 @@ if [[ $(command -v zellij) ]]; then
         }
     fi
 
+    zzd() { zellij d "$@" }
     zzr() {
         zellij d $1
         zellij a -c $1
     }
-
     zzrf() {
         zellij d --force $1
         zellij a -c $1
     }
+
+    # Completions
+    _zellij_sessions() {
+        local -a sessions
+        sessions=(${(f)"$(zellij ls -s 2>/dev/null)"})
+        _describe 'zellij session' sessions
+    }
+
+    _zzd()  { _zellij_sessions }
+    _zza()  { _zellij_sessions }
+    _zzr()  { _zellij_sessions }
+    _zzrf() { _zellij_sessions }
+
+    _zzs() {
+        local state
+        _arguments \
+            '1:SSH host:->host' \
+            '2:remote session:->session'
+        case $state in
+            host)
+                _ssh_hosts
+                ;;
+            session)
+                local host=${words[2]}
+                local sessions
+                sessions=(${(f)"$(ssh "$host" -t "zellij ls -rs" 2>/dev/null | tr -d '\r')"})
+                _describe 'remote session' sessions
+                ;;
+        esac
+    }
+
+    compdef _zzd zzd
+    compdef _zza zza
+    compdef _zzr zzr
+    compdef _zzrf zzrf
+    compdef _zzs zzs
 fi
