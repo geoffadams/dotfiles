02960b58302abbde66d81b1143f4996281499865
Geoff Adams <geoff@dianode.net>
2026-04-23 19:36:23 +0100

    zsh: zellij remote SSH session picker with fzf

--- home/.zsh/rc.d/zellij.zsh
+++ home/.zsh/rc.d/zellij.zsh
@@ -13,13 +13,26 @@ if [[ $(command -v zellij) ]]; then
     if [[ $(command -v fzf) ]]; then
         zza() {
             if [[ -z "$1" ]]; then
-                zellij a -c $(zellij ls -s | fzf)
-            else
-                zellij a -c "$1"
+                session=$(zellij ls -s | fzf --height=~40% --layout=reverse --info=inline --border --margin=1)
             fi
+            zellij a -c "$session"
+        }
+        zzs() {
+            host=$1
+            if [[ -z "$host" ]]; then
+                host=$(grep -E '^Host [^\*]' ~/.ssh/config | awk '{print $2}' | fzf --height=~40% --layout=reverse --info=inline --border --margin=1)
+            fi
+            session=$2
+            if [[ -z "$session" ]]; then
+                session=$(ssh $host -t "zellij ls -s" 2>/dev/null | fzf --height=~40% --layout=reverse --info=inline --border --margin=1)
+            fi
+            ssh $host -t "zellij a -c $session"
         }
     else
         alias zza="zellij a -c"
+        zzs() {
+            ssh $1 -t "zellij a -c $2"
+        }
     fi
 
     zzr() {
