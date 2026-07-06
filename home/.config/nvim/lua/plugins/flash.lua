a20109ee0c98737e484a3e6560c18b968c05b61c
Geoff Adams <geoff@dianode.net>
2026-07-06 14:42:30 +0100

    nvim: add flash.nvim for motion awareness

---
+++ home/.config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,48 @@
+return {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+        {
+            "<Leader>g",
+            mode = { "n", "x", "o" },
+            function()
+                require("flash").jump()
+            end,
+            desc = "Flash",
+        },
+        {
+            "<Leader>G",
+            mode = { "n", "x", "o" },
+            function()
+                require("flash").treesitter()
+            end,
+            desc = "Flash Treesitter",
+        },
+        {
+            "<Leader>r",
+            mode = "o",
+            function()
+                require("flash").remote()
+            end,
+            desc = "Remote Flash",
+        },
+        {
+            "<Leader>R",
+            mode = { "o", "x" },
+            function()
+                require("flash").treesitter_search()
+            end,
+            desc = "Treesitter Search",
+        },
+        {
+            "<c-s>",
+            mode = { "c" },
+            function()
+                require("flash").toggle()
+            end,
+            desc = "Toggle Flash Search",
+        },
+    },
+}
