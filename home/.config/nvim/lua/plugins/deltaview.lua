d9db4d7212b64c4e642c200c8d1d96e2afc5884a
Geoff Adams <geoff.adams@acast.com>
2026-07-11 22:47:01 +0100

    nvim: fix DeltaMenu by disabling line numbers

--- home/.config/nvim/lua/plugins/deltaview.lua
+++ home/.config/nvim/lua/plugins/deltaview.lua
@@ -1,7 +1,6 @@
 return {
     "kokusenz/deltaview.nvim",
     opts = {
-        line_numbers = true,
         keyconfig = {
             dm_toggle_keybind = "<Leader>vD",
             dv_toggle_keybind = "<Leader>vd",
