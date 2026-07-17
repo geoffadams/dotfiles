96a649f160e057dbac90c032919a43ca59349b47
Geoff Adams <geoff.adams@acast.com>
2026-07-17 17:38:45 +0100

    nvim: flash tweaks, mainly just use treesitter mode

--- home/.config/nvim/lua/plugins/flash.lua
+++ home/.config/nvim/lua/plugins/flash.lua
@@ -6,36 +6,20 @@ return {
             "<Leader>g",
             mode = { "n", "x", "o" },
             function()
-                require("flash").jump()
+                require("flash").treesitter()
             end,
-            desc = "Flash",
+            desc = "Jump 🌳",
         },
         {
             "<Leader>G",
             mode = { "n", "x", "o" },
             function()
-                require("flash").treesitter()
+                require("flash").jump()
             end,
-            desc = "Flash Treesitter",
+            desc = "Jump",
         },
         {
-            "<Leader>r",
-            mode = "o",
-            function()
-                require("flash").remote()
-            end,
-            desc = "Remote Flash",
-        },
-        {
-            "<Leader>R",
-            mode = { "o", "x" },
-            function()
-                require("flash").treesitter_search()
-            end,
-            desc = "Treesitter Search",
-        },
-        {
-            "<c-s>",
+            "<C-s>",
             mode = { "c" },
             function()
                 require("flash").toggle()
@@ -46,6 +30,29 @@ return {
     ---@module "flash"
     ---@type Flash.Config
     ---@diagnostic disable: missing-fields
-    opts = {},
+    opts = {
+        label = {
+            before = true,
+            after = false,
+            rainbow = {
+                enabled = true,
+            },
+        },
+        modes = {
+            search = {
+                enabled = true,
+            },
+            treesitter = {
+                label = {
+                    before = true,
+                    after = false,
+                },
+                highlight = {
+                    -- backdrop = true,
+                    -- matches = true,
+                },
+            },
+        },
+    },
     ---@diagnostic enable: missing-fields
 }
