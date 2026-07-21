39d144c220f0291a70b5c01f0cfbabb075554a30
Geoff Adams <geoff.adams@acast.com>
2026-07-21 14:44:29 +0100

    zsh: completion options overhaul

--- home/.zsh/rc.d/completions.zsh
+++ home/.zsh/rc.d/completions.zsh
@@ -5,29 +5,57 @@ if has_brew; then
     fpath+=(${BREW_ZSH_SHARE_PREFIX}/zsh/site-functions)
 fi
 
-# completion menu behaviour
-unsetopt menu_complete
-setopt auto_menu
-setopt list_rows_first
-zstyle ':completion:*' rehash true
+# completion behaviour
+zmodload zsh/complist
+setopt always_to_end
+setopt complete_in_word
+setopt extended_glob
+setopt glob_dots
+setopt glob_complete
+zstyle ':completion:*' keep-prefix true
 
-# completion menu appearance
-zstyle ':completion:*' menu select
+# cache
+zstyle ':completion:*' rehash true
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path "${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompcache"
+
+# completion menu
+unsetopt menu_complete
+setopt auto_list                   # first tab, show list
+setopt auto_menu                   # second tab, show menu
+zstyle ':completion:*' menu select # second tab, pre-select first match
+setopt list_rows_first
+
+# completion menu controls
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect '^xi' vi-insert
+
+# completion styles
+zstyle ':completion:*' completer _expand _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+zstyle ':completion:*' expand prefix
+zstyle ':completion:*' insert-unambiguous
+zstyle ':completion:*' special-dirs false
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-zstyle ':completion:*:(ssh|scp|rsync):*' tag-order ' hosts:-ipaddr:ip\ address hosts:-host:host files'
-zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns loopback ip6-loopback localhost ip6-localhost broadcasthost
+
+zstyle ':completion:*:(ssh|scp|rsync):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
+zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-domain hosts-host hosts-ipaddr
+zstyle ':completion:*:ssh:*' group-order users hosts-domain hosts-host users hosts-ipaddr
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*(.|:)*' loopback ip6-loopback localhost ip6-localhost broadcasthost
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*'
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.0.<->' '255.255.255.255' '::1' 'fe80::*'
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
-# completion matching behaviour
-zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
-setopt complete_in_word
-setopt always_to_end
-setopt glob_complete
-
-# filesystem completions
-setopt globdots
-zstyle ':completion:*' special-dirs false
+# grouping
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
 
 # init completions
 autoload -Uz compinit
