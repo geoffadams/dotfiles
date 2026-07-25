599c00917514a579fd9ebcea8ef0fbecbae85121
Geoff Adams <geoff@dianode.net>
2026-07-25 16:29:54 +0100

    zsh: tune ordering of ssh/scp/rsync completions

--- home/.zsh/rc.d/completions.zsh
+++ home/.zsh/rc.d/completions.zsh
@@ -12,12 +12,21 @@ setopt complete_in_word
 setopt extended_glob
 setopt glob_dots
 setopt glob_complete
-zstyle ':completion:*' keep-prefix true
 
-# cache
+# completers
+zstyle ':completion:*' completer _expand _complete _match _correct _approximate
+zstyle ':completion:*:correct:*' max-errors 1 numeric
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+zstyle ':completion:*' keep-prefix true
+zstyle ':completion:*' expand prefix
+zstyle ':completion:*' insert-unambiguous true
+zstyle ':completion:*' special-dirs false
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
+
+# completer cache
 zstyle ':completion:*' rehash true
-zstyle ':completion::complete:*' use-cache on
-zstyle ':completion::complete:*' cache-path "${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompcache"
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompcache"
 
 # completion menu
 unsetopt menu_complete
@@ -33,24 +42,24 @@ bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect '^xi' vi-insert
 
+# grouping
+zstyle ':completion:*' group-name ''
+
 # completion styles
-zstyle ':completion:*' completer _expand _complete _match _approximate
-zstyle ':completion:*:match:*' original only
-zstyle ':completion:*:approximate:*' max-errors 1 numeric
-zstyle ':completion:*' expand prefix
-zstyle ':completion:*' insert-unambiguous
-zstyle ':completion:*' special-dirs false
-zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 
-zstyle ':completion:*:(ssh|scp|rsync):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
-zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-domain hosts-host hosts-ipaddr
-zstyle ':completion:*:ssh:*' group-order users hosts-domain hosts-host users hosts-ipaddr
-zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*(.|:)*' loopback ip6-loopback localhost ip6-localhost broadcasthost
+zstyle ':completion:*:(ssh|scp|rsync):*:descriptions' format '%F{green}:: %d%f'
+zstyle ':completion:*:(ssh|scp|rsync):*' sort false
+zstyle ':completion:*:(ssh|scp|rsync):*' tag-order 'hosts:-domain:domain hosts:-host:host hosts:-ipaddr:ip\ address files'
+zstyle ':completion:*:(scp|rsync):*' group-order hosts-domain hosts-host hosts-ipaddr files
+zstyle ':completion:*:ssh:*' group-order hosts-domain hosts-host hosts-ipaddr
+
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*'
+zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*(.|:)*' loopback ip6-loopback localhost ip6-localhost broadcasthost
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.0.<->' '255.255.255.255' '::1' 'fe80::*'
-zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+zstyle -e ':completion:*:(ssh|scp|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
 # init completions
 autoload -Uz compinit
