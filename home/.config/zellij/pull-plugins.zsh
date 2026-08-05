c04c27bf40bcfae49b9e49290a01ebf65bac14d8
Geoff Adams <geoff.adams@acast.com>
2026-08-05 11:14:29 +0100

    zellij: relocate plugins directory under ~/.local/share

--- home/.config/zellij/pull-plugins.zsh
+++ home/.config/zellij/pull-plugins.zsh
@@ -1,5 +1,14 @@
-cd plugins
-wget -N "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm"
-wget -N "https://github.com/dj95/zjstatus/releases/latest/download/zjframes.wasm"
-wget -N "https://github.com/karimould/zellij-forgot/releases/latest/download/zellij_forgot.wasm"
-wget -N "https://github.com/liam-mackie/zsm/releases/latest/download/zsm.wasm"
+#!/usr/bin/env zsh
+
+ZELLIJ_PLUGIN_DIR="$HOME/.local/share/zellij/plugins"
+
+mkdir -p "$ZELLIJ_PLUGIN_DIR"
+cd "$ZELLIJ_PLUGIN_DIR"
+
+function fetch_plugin() {
+    wget -N $@
+}
+fetch_plugin "https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm"
+fetch_plugin "https://github.com/dj95/zjstatus/releases/latest/download/zjframes.wasm"
+fetch_plugin "https://github.com/karimould/zellij-forgot/releases/latest/download/zellij_forgot.wasm"
+fetch_plugin "https://github.com/liam-mackie/zsm/releases/latest/download/zsm.wasm"
